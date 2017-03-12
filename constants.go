0ebe79da0ccea9c63c781bf2251bcea84665218f
konstantin8105 <konstantin8105@gmail.com>
2017-03-12 22:36:53 +0300

    godoc

--- constants.go
+++ constants.go
@@ -12,4 +12,6 @@ func main() {
 	fmt.Println("Hello")
 	s := section.Ibeam{H: 0.4, B: 0.4, Tf: 0.030, Tw: 0.02}
 	fmt.Println("Section = ", s, "\t Jx = ", s.Jx())
+	plate := section.Plate{Height: 0.080 /* meter */, Thickness: 0.008 /* meter */}
+	fmt.Printf("Area of plate is %.5e m^2\n", plate.Area())
 }
--- section/section_test.go
+++ section/section_test.go
@@ -10,3 +10,10 @@ func Example() {
 	fmt.Printf("Moment inertia of I-beam by axe X is %.4e m^-4", s.Jx())
 	// Output: Moment inertia of I-beam by axe X is 2.3798e-06 m^-4
 }
+
+// This is a simple example
+func ExamplePlate() {
+	plate := Plate{Height: 0.080 /* meter */, Thickness: 0.008 /* meter */}
+	fmt.Printf("Area of plate is %.1e m^2\n", plate.Area())
+	// Output: Area of plate is 6.4e-4 m^2
+}
